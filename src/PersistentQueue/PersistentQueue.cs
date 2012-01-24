3fc62e06233614a151fc004bb06d43a4516d6b1a
Victor Arias <arias.sp@gmail.com>
2012-01-24 14:38:34 -0200

    very basic funcionality

---
+++ src/PersistentQueue/PersistentQueue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PersistentQueue
+{
+	public class Queue
+	{
+		private Queue<object> itens = new Queue<object>();
+
+		public void Enqueue(object obj)
+		{
+			itens.Enqueue(obj);
+		}
+
+		public object Dequeue()
+		{
+			return itens.Dequeue();
+		}
+
+		public T Dequeue<T>()
+		{
+			return (T)Dequeue();
+		}
+
+		public object Peek()
+		{
+			return itens.Peek();
+		}
+
+		public T Peek<T>()
+		{
+			return (T)Peek();
+		}
+	}
+
+	public class Item
+	{
+		public long Id { get; private set; }
+		public byte[] Message { get; set; }
+
+		public Item()
+		{
+			Id = DateTime.Now.Ticks;
+		}
+	}
+}
---
+++ src/Tests/PersistentQueueTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using PersistentQueue;
+using FluentAssertions;
+
+namespace Tests
+{
+	[TestFixture]
+	public class PersistentQueueTests
+	{
+		[Test]
+		public void ShouldQueueAndDequeueString()
+		{
+			var queue = new Queue();
+			var item = "woot";
+
+			queue.Enqueue(item);
+
+			string dequeued = queue.Dequeue<string>();
+			dequeued.Should().Be(item);
+		}
+
+		[Test]
+		public void ShouldQueueAndPeekString()
+		{
+			var queue = new Queue();
+			var item = "woot";
+
+			queue.Enqueue(item);
+
+			var peeked = queue.Peek();
+			peeked.Should().Be(item);
+		}
+	}
+}
